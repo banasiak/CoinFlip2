36085e455050d1405a48844502bc92c696779ae2
banasiak <richard@banasiak.com>
2024-01-03 18:08:17 -0700

    ktlint

--- coinflip/build.gradle.kts
+++ coinflip/build.gradle.kts
@@ -38,6 +38,8 @@ android {
   }
 }
 
+val ktlint by configurations.creating
+
 dependencies {
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("androidx.core:core-ktx:1.12.0")
@@ -50,8 +52,46 @@ dependencies {
   implementation("com.google.dagger:hilt-android:2.50")
   implementation("com.jakewharton.timber:timber:5.0.1")
   implementation("com.squareup:seismic:1.0.3")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
   ksp("com.google.dagger:dagger-compiler:2.50")
   ksp("com.google.dagger:hilt-android-compiler:2.50")
   testImplementation("junit:junit:4.13.2")
+
+  ktlint("com.pinterest.ktlint:ktlint-cli:1.1.0") {
+    attributes {
+      attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+    }
+  }
+}
+
+val ktlintCheck by tasks.registering(JavaExec::class) {
+  group = LifecycleBasePlugin.VERIFICATION_GROUP
+  description = "Check Kotlin code style"
+  classpath = ktlint
+  mainClass.set("com.pinterest.ktlint.Main")
+  // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+  args(
+    "**/src/**/*.kt",
+    "**.kts",
+    "!**/build/**"
+  )
+}
+
+tasks.check {
+  dependsOn(ktlintCheck)
+}
+
+tasks.register<JavaExec>("format") {
+  group = LifecycleBasePlugin.VERIFICATION_GROUP
+  description = "Check Kotlin code style and format"
+  classpath = ktlint
+  mainClass.set("com.pinterest.ktlint.Main")
+  jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+  // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+  args(
+    "-F",
+    "**/src/**/*.kt",
+    "**.kts",
+    "!**/build/**"
+  )
 }
